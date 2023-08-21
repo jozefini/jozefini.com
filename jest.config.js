4fc1af1df9486d1759282418349aff206351a2db
jozefini <jozibashaj@gmail.com>
2023-08-21 22:43:24 +0200

    [ stage ] fix lint

--- jest.config.js
+++ jest.config.js
@@ -3,9 +3,5 @@ module.exports = {
 	testEnvironment: 'node',
 	moduleNameMapper: {
 		'^@/(.*)$': '<rootDir>/$1',
-		'^$app/(.*)$': '<rootDir>/app/$1',
-		'^$components/(.*)$': '<rootDir>/components/$1',
-		'^$hooks/(.*)$': '<rootDir>/hooks/$1',
-		'^$services/(.*)$': '<rootDir>/services/$1',
 	},
 }
