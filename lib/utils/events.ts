792db184c16758768515700be16a9632a67c3996
jozefini <jozibashaj@gmail.com>
2023-09-01 16:52:38 +0200

    Update structure

---
+++ lib/utils/events.ts
@@ -0,0 +1,11 @@
+import { ElementEvent } from '$/lib/types'
+
+// Register an event listener on an element
+export const on: ElementEvent = (el, event, handler) => {
+	el?.addEventListener(event, handler)
+}
+
+// Remove an event listener from an element
+export const off: ElementEvent = (el, event, handler) => {
+	el?.removeEventListener(event, handler)
+}
