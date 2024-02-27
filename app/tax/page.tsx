2610139c6252d6c790632b3c949f75018173a8ea
jozefini <jozibashaj@gmail.com>
2024-02-27 21:06:00 +0100

    tax

--- app/tax/page.tsx
+++ app/tax/page.tsx
@@ -3,19 +3,27 @@
 import { useEffect, useMemo, useState } from 'react'
 import { getPercentInValue, getValueInPercent, valueAsPrice } from './lib/utils'
 import { Input } from './ui/input'
+import { cn } from '@/lib/utils'
 
 const css = {
-  wrapper: 'fixed inset-0 bg-orange-900/10',
-  inner: 'max-w-xl mx-auto px-4 py-8 min-w-0',
-  group: 'min-w-0 flex items-center justify-center gap-x-4',
-  separator: 'border-t border-orange-800 my-8 border-dashed',
-  table: 'grid grid-cols-2 gap-x-14 *:first:text-left *:last:text-right',
+  wrapper: 'fixed inset-0 bg-[#fef6f0]',
+  inner: 'max-w-96 sm:max-w-xl w-full mx-auto px-8 py-5 sm:py-8 min-w-0',
+  group:
+    'min-w-0 flex flex-col sm:flex-row gap-y-3 items-center justify-center gap-x-4',
+  separatorWrap:
+    'relative h-12 sm:h-14 flex items-center justify-center w-full',
+  separator: 'absolute left-0 top-1/2 border-t border-orange-800 w-full',
+  separatorText:
+    'relative inline-flex px-2 bg-[#fef6f0] text-xs uppercase font-bold text-orange-800 tracking-widest',
+  table: 'grid gap-y-8 grid-cols-1 gap-x-6 *:first:text-left *:last:text-right',
+  doubleCol: 'sm:grid-cols-2',
   detail: 'flex justify-between items-center',
   detailSeparator: 'border-t border-orange-800 my-2',
   value: 'text-base font-bold text-orange-950',
   label: 'text-xs uppercase font-bold text-orange-800 tracking-widest',
   headline: 'text-xs font-bold text-orange-800 uppercase tracking-widest',
-  endResult: 'border border-dashed border-orange-800 p-4 max-w-64 mx-auto',
+  endResult:
+    'border bg-white/50 w-full border-dashed border-orange-800 p-4 sm:max-w-64 mx-auto',
 }
 
 const TAX = 15
@@ -24,12 +32,23 @@ const INCOME = 0
 const EXPENSES = 0
 const MAX_INCOME = 140000
 
+const Separator = ({ title }: { title: string }) => {
+  return (
+    <div className={css.separatorWrap}>
+      <div className={css.separator} />
+      <div className={css.separatorText}>{title}</div>
+    </div>
+  )
+}
+
 export default function Page() {
   const [tax, setTax] = useState(TAX)
   const [income, setIncome] = useState(INCOME)
   const [expenses, setExpenses] = useState(EXPENSES)
 
   const hasBigTax = income >= MAX_INCOME
+  const hasExpenses = expenses > 0
+
   useEffect(() => {
     setTax(hasBigTax ? BIG_TAX : TAX)
   }, [hasBigTax])
@@ -42,16 +61,18 @@ export default function Page() {
     taxWithoutExpenses,
     profitWithoutExpenses,
     taxDifference,
-    profitDifference,
   } = useMemo(() => {
     const incomeWithExpenses = income - expenses
     const taxWithExpenses = getValueInPercent(incomeWithExpenses, tax)
     const profitWithExpenses = incomeWithExpenses - taxWithExpenses
     const incomeWithoutExpenses = income
     const taxWithoutExpenses = getValueInPercent(incomeWithoutExpenses, tax)
-    const profitWithoutExpenses = incomeWithoutExpenses - taxWithoutExpenses
-    const taxDifference = taxWithExpenses - taxWithoutExpenses
-    const profitDifference = profitWithoutExpenses - profitWithExpenses
+    const profitWithoutExpenses = Number(
+      (incomeWithoutExpenses - taxWithoutExpenses).toFixed(0)
+    )
+    const taxDifference = Number(
+      (taxWithExpenses - taxWithoutExpenses).toFixed(0)
+    )
 
     return {
       incomeWithExpenses,
@@ -61,7 +82,6 @@ export default function Page() {
       taxWithoutExpenses,
       profitWithoutExpenses,
       taxDifference,
-      profitDifference,
     }
   }, [tax, income, expenses])
 
@@ -69,44 +89,21 @@ export default function Page() {
     <div className={css.wrapper}>
       <div className={css.inner}>
         <div className={css.group}>
-          <Input label="Tax" value={tax} onChange={() => {}} disabled />
+          <Input
+            label="Tax"
+            value={tax}
+            onChange={() => {}}
+            disabled
+            symbol="%"
+          />
           <Input label="Income" value={income} onChange={setIncome} />
           <Input label="Expenses" value={expenses} onChange={setExpenses} />
         </div>
-        <div className={css.separator} />
-        <div className={css.table}>
-          <div>
+        <Separator title="With / Without Expenses" />
+        <div className={cn(css.table, hasExpenses && css.doubleCol)}>
+          <div className={css.endResult}>
             <div className={css.detail}>
-              <div className={css.headline}>With Expenses</div>
-            </div>
-            <div className={css.detailSeparator} />
-            <div className={css.detail}>
-              <label className={css.label}>Income</label>
-              <span className={css.value}>
-                {valueAsPrice(incomeWithExpenses)}
-              </span>
-            </div>
-            <div className={css.detail}>
-              <label className={css.label}>Tax</label>
-              <span className={css.value}>
-                {valueAsPrice(-taxWithExpenses)}
-              </span>
-            </div>
-            <div className={css.detailSeparator} />
-            <div className={css.detail}>
-              <label className={css.label}>Profit</label>
-              <span className={css.value}>
-                {valueAsPrice(profitWithExpenses)}
-              </span>
-            </div>
-          </div>
-          <div>
-            <div className={css.detail}>
-              <div className={css.headline}>Without Expenses</div>
-            </div>
-            <div className={css.detailSeparator} />
-            <div className={css.detail}>
-              <label className={css.label}>Income</label>
+              <label className={css.label}>Balance</label>
               <span className={css.value}>
                 {valueAsPrice(incomeWithoutExpenses)}
               </span>
@@ -125,33 +122,54 @@ export default function Page() {
               </span>
             </div>
           </div>
-        </div>
-        {expenses > 0 && (
-          <>
-            <div className={css.separator} />
+          {hasExpenses && (
             <div className={css.endResult}>
               <div className={css.detail}>
-                <label className={css.label}>Tax Off</label>
-                <span className={css.value}>{valueAsPrice(taxDifference)}</span>
+                <label className={css.label}>Balance</label>
+                <span className={css.value}>
+                  {valueAsPrice(incomeWithExpenses)}
+                </span>
               </div>
               <div className={css.detail}>
-                <label className={css.label}>% Off</label>
+                <label className={css.label}>Tax</label>
                 <span className={css.value}>
-                  {getValueInPercent(tax, taxDifference)} %
+                  {valueAsPrice(-taxWithExpenses)}
                 </span>
               </div>
               <div className={css.detailSeparator} />
               <div className={css.detail}>
-                <label className={css.label}>New Tax</label>
+                <label className={css.label}>Profit</label>
                 <span className={css.value}>
-                  {valueAsPrice(tax - taxDifference)}
+                  {valueAsPrice(profitWithExpenses)}
                 </span>
               </div>
-              <div className={css.detail}>
-                <label className={css.label}>Expenses ~</label>
-                <span className={css.value}>
-                  {valueAsPrice(profitDifference)}
-                </span>
+            </div>
+          )}
+        </div>
+        {hasExpenses && (
+          <>
+            <Separator title="Tax difference" />
+            <div className={css.table}>
+              <div className={css.endResult}>
+                <div className={css.detail}>
+                  <label className={css.label}>Tax</label>
+                  <span className={css.value}>
+                    {valueAsPrice(taxWithoutExpenses)}
+                  </span>
+                </div>
+                <div className={css.detail}>
+                  <label className={css.label}>Tax Off</label>
+                  <span className={css.value}>
+                    {valueAsPrice(taxDifference)}
+                  </span>
+                </div>
+                <div className={css.detailSeparator} />
+                <div className={css.detail}>
+                  <label className={css.label}>New Tax</label>
+                  <span className={css.value}>
+                    {valueAsPrice(tax - taxDifference)}
+                  </span>
+                </div>
               </div>
             </div>
           </>
--- app/tax/ui/input.tsx
+++ app/tax/ui/input.tsx
@@ -2,14 +2,14 @@ import { cn } from '@/lib/utils'
 import { useId } from 'react'
 
 const css = {
-  wrapper: 'min-w-0 max-w-full',
+  wrapper: 'min-w-0 max-w-full w-full',
   headline: '',
   label: 'text-xs uppercase font-bold text-orange-800 tracking-wider',
   control: 'relative',
   input:
-    'inline-flex w-full text-xl items-center h-12 leading-tight pl-8 pr-2 py-2 border-2 rounded-lg bg-orange-50/50 border-orange-900 text-orange-950 font-semibold focus:outline-none disabled:pointer-events-none disabled:select-none disabled:bg-orange-800/10',
+    'inline-flex w-full text-xl items-center h-12 leading-tight pl-9 pr-2 py-2 border-2 rounded-lg bg-white border-orange-900 text-orange-950 font-semibold focus:outline-none disabled:pointer-events-none disabled:select-none disabled:bg-[#fef6f0]',
   symbol:
-    'absolute inset-y-0 left-0 flex items-center pl-2 text-xl pointer-events-none font-semibold text-orange-800',
+    'absolute inset-y-0 left-0 flex items-center pl-3.5 text-xl pointer-events-none font-semibold text-orange-800',
 }
 type InputProps = Omit<
   React.InputHTMLAttributes<HTMLInputElement>,
